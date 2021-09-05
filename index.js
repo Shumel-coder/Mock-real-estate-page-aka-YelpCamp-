93dd786e1835f6653d59416461c85cc6194f13fb
Shumel <captainshumelz@gmail.com>
2021-09-05 12:24:13 -0400

    fixed error

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/shumel-realestate'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/shumelrealestate'
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
